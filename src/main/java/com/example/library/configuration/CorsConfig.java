12ef00a1a5d8caad20f95a683213a86278a3e72b
Zaahid <mzra@ic.sg>
2025-07-10 16:17:05 +0700

    Update CORS

--- src/main/java/com/example/library/configuration/CorsConfig.java
+++ src/main/java/com/example/library/configuration/CorsConfig.java
@@ -13,9 +13,13 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:3000") // ganti sesuai port React kamu
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedHeaders("*");
+                        .allowedOrigins(
+                                "http://localhost:3000",
+                                "https://library-fe.vercel.app"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
