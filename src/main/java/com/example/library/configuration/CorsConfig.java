2b89cde2c95cd810b5b7f54f674f65386e354075
Zaahid <mzra@ic.sg>
2025-07-10 16:32:50 +0700

    Update CORS

--- src/main/java/com/example/library/configuration/CorsConfig.java
+++ src/main/java/com/example/library/configuration/CorsConfig.java
@@ -13,10 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins(
-                                "http://localhost:3000",
-                                "https://library-ui-gamma.vercel.app"
-                        )
+                        .allowedOrigins("*")
                         .allowedMethods("*")
                         .allowedHeaders("*")
                         .allowCredentials(true);
